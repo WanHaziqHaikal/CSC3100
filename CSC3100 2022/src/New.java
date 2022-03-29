10742e602325fad91992d30a3de135ac529f280c
User <User@LAPTOP-UH09AJJU>
2022-03-29 09:20:22 +0800

    tambah

--- CSC3100 2022/src/New.java
+++ CSC3100 2022/src/New.java
@@ -1,4 +1,80 @@
 
 public class New {
+ public static void main (String[] args) {
+	 
+	 
+	 
+	 double radius =20;
+	 double PI = 3.14159;
+	double area = radius*radius*PI;
+	
+	System.out.println ( area);
+	 
+	 
+	 
+	 
+ }
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
 
+ }
+	/* int i= 10;
+	 int newNum= 10 * i++ ;
+	 
+	 System.out.println ( newNum );
+	 
+	 System.out.println ( i );
+	 
+	 
+	 
+ }*/
+
+
+	
+/*	int i= 10;
+	 int newNum= 10 * ++i;
+	 
+System.out.println ( newNum );
+	 
+	 System.out.println ( i );
 }
+           
+}*/
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
